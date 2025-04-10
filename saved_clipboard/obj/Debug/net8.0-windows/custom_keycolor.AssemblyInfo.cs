19932bcae238f9c72c36d0a5eb9683753c1766ca
Yildiz Kabaran <yildizkabaran@gmail.com>
2025-04-10 14:58:23 +0300

    updates

--- saved_clipboard/obj/Debug/net8.0-windows/custom_keycolor.AssemblyInfo.cs
+++ saved_clipboard/obj/Debug/net8.0-windows/custom_keycolor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("custom_keycolor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dad65ada705a27279b8a1690a7abea7c810bacf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a2e1b7e649dec4613b8ae930db4026108e8fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("custom_keycolor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("custom_keycolor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
