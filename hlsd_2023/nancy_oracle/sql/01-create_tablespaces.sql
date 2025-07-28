7245e8c7024a2c75527e75570c075a0c390ebf1a
Yildiz Kabaran <yildizkabaran@gmail.com>
2025-07-28 14:24:22 +0300

    updates

--- hlsd_2023/nancy_oracle/sql/01-create_tablespaces.sql
+++ hlsd_2023/nancy_oracle/sql/01-create_tablespaces.sql
@@ -36,120 +36,95 @@ alter database drop logfile group 2;
 alter database drop logfile group 3;
 
 create tablespace app_log
-	datafile '/uoradata/NANCY/app_log01.dbf' size 8G autoextend off
+	datafile '/uoradata/NANCY/app_log01.dbf' size 16G autoextend off
 	NOLOGGING;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log02.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log02.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log03.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log03.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log04.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log04.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log05.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log05.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log06.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log06.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log07.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log07.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log08.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log08.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log09.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log09.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log10.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log10.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log11.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log11.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log12.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log12.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log13.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log13.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log14.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log14.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log15.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log15.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log16.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log16.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log17.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log17.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log18.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log18.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log19.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log19.dbf' size 16G autoextend off;
 alter tablespace app_log
-   add datafile '/uoradata/NANCY/app_log20.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log20.dbf' size 16G autoextend off;
 
 create tablespace app_log_index
-	datafile '/uoradata/NANCY/app_log_index01.dbf' size 8G autoextend off
+	datafile '/uoradata/NANCY/app_log_index01.dbf' size 16G autoextend off
    NOLOGGING;
 alter tablespace app_log_index
-	add datafile '/uoradata/NANCY/app_log_index02.dbf' size 8G autoextend off;
+	add datafile '/uoradata/NANCY/app_log_index02.dbf' size 16G autoextend off;
 alter tablespace app_log_index
-	add datafile '/uoradata/NANCY/app_log_index03.dbf' size 8G autoextend off;
+	add datafile '/uoradata/NANCY/app_log_index03.dbf' size 16G autoextend off;
 alter tablespace app_log_index
-   add datafile '/uoradata/NANCY/app_log_index04.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log_index04.dbf' size 16G autoextend off;
 alter tablespace app_log_index
-   add datafile '/uoradata/NANCY/app_log_index05.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log_index05.dbf' size 16G autoextend off;
 alter tablespace app_log_index
-   add datafile '/uoradata/NANCY/app_log_index06.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log_index06.dbf' size 16G autoextend off;
 alter tablespace app_log_index
-   add datafile '/uoradata/NANCY/app_log_index07.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log_index07.dbf' size 16G autoextend off;
 alter tablespace app_log_index
-   add datafile '/uoradata/NANCY/app_log_index08.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log_index08.dbf' size 16G autoextend off;
 alter tablespace app_log_index
-   add datafile '/uoradata/NANCY/app_log_index09.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log_index09.dbf' size 16G autoextend off;
 alter tablespace app_log_index
-   add datafile '/uoradata/NANCY/app_log_index10.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/app_log_index10.dbf' size 16G autoextend off;
+alter tablespace app_log_index
+   add datafile '/uoradata/NANCY/app_log_index11.dbf' size 16G autoextend off;
+alter tablespace app_log_index
+   add datafile '/uoradata/NANCY/app_log_index12.dbf' size 16G autoextend off;
 
 create tablespace feed_file
-   datafile '/uoradata/NANCY/feed_file01.dbf' size 8G autoextend off
+   datafile '/uoradata/NANCY/feed_file01.dbf' size 16G autoextend off
    NOLOGGING;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file02.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file02.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file03.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file03.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file04.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file04.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file05.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file05.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file06.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file06.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file07.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file07.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file08.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file08.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file09.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file09.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file10.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file10.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file11.dbf' size 8G autoextend off;
+   add datafile '/uoradata/NANCY/feed_file11.dbf' size 16G autoextend off;
 alter tablespace feed_file
-   add datafile '/uoradata/NANCY/feed_file12.dbf' size 8G autoextend off;
-
-select
-   fs.tablespace_name                          "Tablespace",
-   (df.totalspace - fs.freespace)              "Used MB",
-   fs.freespace                                "Free MB",
-   df.totalspace                               "Total MB",
-   df.MaxSpace                               "Max MB",
-   round(100 * (fs.freespace / df.totalspace)) "Curr Pct. Free",
-   round(100 * ((df.MaxSpace - (df.totalspace - fs.freespace)) / df.MaxSpace)) "Max Pct. Free"
-from
-   (select
-      tablespace_name,
-      round(sum(bytes) / 1048576) TotalSpace,
-      round(sum(decode(nvl(maxbytes,0),0, bytes,maxbytes)) / 1048576) MaxSpace
-   from
-      dba_data_files
-   group by
-      tablespace_name
-   ) df,
-   (select
-      tablespace_name,
-      round(sum(bytes) / 1048576) FreeSpace
-   from
-      dba_free_space
-   group by
-      tablespace_name
-   ) fs
-where
-   df.tablespace_name = fs.tablespace_name;
+   add datafile '/uoradata/NANCY/feed_file12.dbf' size 16G autoextend off;
